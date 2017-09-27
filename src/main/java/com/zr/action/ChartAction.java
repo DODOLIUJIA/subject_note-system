cd0e89fbc1a2fa03fa10808287dd8cf9482e6df3
Zxy <1060676865@qq.com>
2017-09-27 22:42:52 +0800

    927

---
+++ src/main/java/com/zr/action/ChartAction.java
@@ -0,0 +1,34 @@
+package com.zr.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.zr.service.CommentService;
+import com.zr.service.SubLabelService;
+import com.zr.service.impl.CommentServiceImpl;
+import com.zr.service.impl.SubLabelServiceImpl;
+
+import net.sf.json.JSONArray;
+
+@WebServlet("/chart")
+public class ChartAction extends HttpServlet {
+	SubLabelService sub_label = new SubLabelServiceImpl();
+	CommentService cs = new CommentServiceImpl();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		JSONArray result = sub_label.getSubLabelsCount();
+		System.out.println(result.toString());
+		response.getWriter().write(result.toString());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
