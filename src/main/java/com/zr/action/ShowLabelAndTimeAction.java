24fc877113b2768d343402eb26b55bf5b87ec352
Administrator <Administrator@PC201405231421>
2017-09-27 09:12:22 +0800

    ws927

---
+++ src/main/java/com/zr/action/ShowLabelAndTimeAction.java
@@ -0,0 +1,58 @@
+package com.zr.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.zr.model.Subject;
+import com.zr.model.SubjectLabel;
+import com.zr.service.SubService;
+import com.zr.service.impl.SubServiceImpl;
+
+import net.sf.json.JSONObject;
+
+/**
+ * Servlet implementation class ShowLabelAndTimeAction
+ */
+@WebServlet("/showLabelAndTime")
+public class ShowLabelAndTimeAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	SubService sbs = new SubServiceImpl();
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowLabelAndTimeAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<SubjectLabel> labels = sbs.selectSubLabel();
+		List<Subject> times = sbs.selectSubTime();
+		HttpSession session = request.getSession();
+		session.setAttribute("labels", labels);
+		session.setAttribute("times", times);
+		PrintWriter pw = response.getWriter();
+		JSONObject j = new JSONObject();
+		pw.write(j.toString());
+	}
+
+}
---
+++ src/main/java/com/zr/model/SubjectLabel.java
@@ -0,0 +1,19 @@
+package com.zr.model;
+
+public class SubjectLabel {
+private int s_lid;
+private String s_lname;
+public int getS_lid() {
+	return s_lid;
+}
+public void setS_lid(int s_lid) {
+	this.s_lid = s_lid;
+}
+public String getS_lname() {
+	return s_lname;
+}
+public void setS_lname(String s_lanem) {
+	this.s_lname = s_lanem;
+}
+
+}
